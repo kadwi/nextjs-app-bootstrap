a7e18bcb233a080213b2ff3241a391fe1b430e3b
kadwi <36479042+kadwi@users.noreply.github.com>
2025-06-06 15:22:15 +0800

    Add .next/static/chunks/src_app_[email]_page_tsx_a1f9e366._.js 1749194535360

---
+++ .next/static/chunks/src_app_[email]_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_bea5becd._.js",
+  "static/chunks/src_358e4292._.js"
+],
+    source: "dynamic"
+});
